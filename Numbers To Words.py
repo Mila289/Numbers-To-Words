bb3c0958e1fd6433435e344e17a0dd72aa79548d
Mila289 <mila.krasulina@gmail.com>
2025-01-02 19:25:24 +0700

    Обработка исключений.

--- Numbers To Words.py
+++ Numbers To Words.py
@@ -16,3 +16,36 @@ tens = (
 suffixes = (
    '', 'Thousand', 'Million', 'Billion'
    )
+
+
+def fetch_words(number, index):
+   if number == '0': return 'Zero'
+
+   number = number.zfill(3)
+   hundreds_digit = int(number[0])
+   tens_digit = int(number[1])
+   ones_digit = int(number[2])
+
+   words = '' if number[0] == '0' else ones[hundreds_digit]
+
+   if words != '':
+      words += ' Hundred '
+
+   if tens_digit > 1:
+      words += tens[tens_digit - 2]
+      words += ' '
+      words += ones[ones_digit]
+   elif (tens_digit == 1):
+      words += twos[((tens_digit + ones_digit) % 10) - 1]
+   elif (tens_digit == 0):
+      words += ones[ones_digit]
+
+   if (words.endswith('Zero')):
+      words = words[:-len('Zero')]
+   else:
+      words += ' '
+
+   if len(words) != 0:
+      words += suffixes[index]
+
+   return words
