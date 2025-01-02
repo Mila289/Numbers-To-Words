c7af8e4a410288b5f2c2503ab640db52ec1e7a1b
Mila289 <mila.krasulina@gmail.com>
2025-01-02 19:24:07 +0700

    Ввод чисел.

---
+++ Numbers To Words.py
@@ -0,0 +1,18 @@
+ones = (
+   'Zero', 'One', 'Two', 'Three', 'Four',
+   'Five', 'Six', 'Seven', 'Eight', 'Nine'
+   )
+
+twos = (
+   'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen',
+    'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'
+   )
+
+tens = (
+   'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty',
+    'Seventy', 'Eighty', 'Ninety', 'Hundred'
+   )
+
+suffixes = (
+   '', 'Thousand', 'Million', 'Billion'
+   )
